96a4b9ae8f5132e25e19d699af3b00f9b9b84b33
Stepan Manaenko <u4zarus@gmail.com>
2024-09-16 18:49:08 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/AI-Chatbot-Mockup">
-        <React.StrictMode>
+    <React.StrictMode>
+        <HashRouter>
             <App />
-        </React.StrictMode>
-    </BrowserRouter>
+        </HashRouter>
+    </React.StrictMode>
 );
